8722d171e0bdf759ea196003c23be36fc2e26108
OleSTEEP <krinkinoleg@gmail.com>
2022-07-08 15:43:47 +0300

    Reduced cherry tree spawn rate

--- src/main/java/com/olesteep/turorudi/world/feature/TuroPlacedFeatures.java
+++ src/main/java/com/olesteep/turorudi/world/feature/TuroPlacedFeatures.java
@@ -11,7 +11,7 @@ import net.minecraft.world.level.levelgen.placement.RarityFilter;
 public class TuroPlacedFeatures {
     public static final Holder<PlacedFeature> CHERRY_PLACED = PlacementUtils.register("cherry_placed",
             TuroConfiguredFeatures.CHERRY_SPAWN, VegetationPlacements.treePlacement(
-                    PlacementUtils.countExtra(0, 0.05f, 1)));
+                    PlacementUtils.countExtra(0, 0.005f, 1)));
 
     public static final Holder<PlacedFeature> LEMON_BUSH_PLACED = PlacementUtils.register("lemon_bush_placed",
             TuroConfiguredFeatures.LEMON_BUSH, RarityFilter.onAverageOnceEvery(16),

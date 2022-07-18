c50be7c440ddfc5b17f37e2219e1867ba81956ba
OleSTEEP <krinkinoleg@gmail.com>
2022-07-18 15:12:50 +0300

    Tree chance spawn changed, again

--- src/main/java/com/olesteep/turorudi/world/feature/TuroPlacedFeatures.java
+++ src/main/java/com/olesteep/turorudi/world/feature/TuroPlacedFeatures.java
@@ -19,7 +19,7 @@ public class TuroPlacedFeatures {
 
     public static final Holder<PlacedFeature> CHERRY_PLACED = PlacementUtils.register("cherry_placed",
             TuroConfiguredFeatures.CHERRY_SPAWN, VegetationPlacements.treePlacement(
-                    PlacementUtils.countExtra(0, 0.05f, 1)));
+                    PlacementUtils.countExtra(0, 0.025f, 1)));
 
     public static final Holder<PlacedFeature> LEMON_BUSH_PLACED = PlacementUtils.register("lemon_bush_placed",
             TuroConfiguredFeatures.PATCH_LEMON_BUSH, RarityFilter.onAverageOnceEvery(16),
@@ -27,5 +27,5 @@ public class TuroPlacedFeatures {
 
     public static final Holder<PlacedFeature> ORANGE_PLACED = PlacementUtils.register("orange_placed",
             TuroConfiguredFeatures.ORANGE_SPAWN, VegetationPlacements.treePlacement(
-                    PlacementUtils.countExtra(0, 0.05f, 1)));
+                    PlacementUtils.countExtra(0, 0.025f, 1)));
 }

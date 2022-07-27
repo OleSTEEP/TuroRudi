3144a51eae25d25c37d5554380ac142608e1a1bf
OleSTEEP <krinkinoleg@gmail.com>
2022-07-27 13:32:30 +0300

    Block properties

--- src/main/java/com/olesteep/turorudi/block/TuroBlocks.java
+++ src/main/java/com/olesteep/turorudi/block/TuroBlocks.java
@@ -19,8 +19,8 @@ import static com.olesteep.turorudi.event.BlockRegisters.registerBlockWithoutIte
 public class TuroBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TuroRudi.MOD_ID);
 
-    public static final RegistryObject<Block> COCONUT_BLOCK = registerBlockWithoutItem(BLOCKS, "coconut", () -> new TuroCoconutBlock(BlockBehaviour.Properties.copy(Blocks.COCOA)));
-    public static final RegistryObject<Block> BANANA_BLOCK = registerBlockWithoutItem(BLOCKS, "banana", () -> new TuroBananaBlock(BlockBehaviour.Properties.copy(Blocks.COCOA)));
+    public static final RegistryObject<Block> COCONUT_BLOCK = registerBlockWithoutItem(BLOCKS, "coconut", () -> new TuroCoconutBlock(BlockBehaviour.Properties.copy(Blocks.COCOA).randomTicks().strength(0.2F, 3.0F).sound(SoundType.WOOD).noOcclusion()));
+    public static final RegistryObject<Block> BANANA_BLOCK = registerBlockWithoutItem(BLOCKS, "banana", () -> new TuroBananaBlock(BlockBehaviour.Properties.copy(Blocks.COCOA).randomTicks().strength(0.2F, 3.0F).sound(SoundType.WOOL).noOcclusion()));
     public static final RegistryObject<Block> MILK_FAT_BLOCK = registerBlock(BLOCKS, "milk_fat_block", () -> new Block(BlockBehaviour.Properties.of(Material.CAKE).strength(0.8F).sound(SoundType.SLIME_BLOCK)));
     public static final RegistryObject<Block> TURO_BLOCK = registerBlock(BLOCKS, "turorudi_block", () -> new Block(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
     public static final RegistryObject<Block> TURO_BAD_BLOCK = registerBlock(BLOCKS, "turorudi_bad_block", () -> new Block(BlockBehaviour.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));

56bc363712c7bf74d2c559631b74b6042cd3da52
OleSTEEP <krinkinoleg@gmail.com>
2022-08-03 19:43:23 +0300

    PalmFoliagePlacer code

--- src/main/java/com/olesteep/turorudi/world/feature/foliageplacers/PalmFoliagePlacer.java
+++ src/main/java/com/olesteep/turorudi/world/feature/foliageplacers/PalmFoliagePlacer.java
@@ -15,9 +15,7 @@ import java.util.Random;
 import java.util.function.BiConsumer;
 
 public class PalmFoliagePlacer extends FoliagePlacer {
-    public static final Codec<PalmFoliagePlacer> CODEC = RecordCodecBuilder.create((p_68380_) -> {
-        return foliagePlacerParts(p_68380_).apply(p_68380_, PalmFoliagePlacer::new);
-    });
+    public static final Codec<PalmFoliagePlacer> CODEC = RecordCodecBuilder.create((p_68380_) -> foliagePlacerParts(p_68380_).apply(p_68380_, PalmFoliagePlacer::new));
 
     public PalmFoliagePlacer(IntProvider p_161343_, IntProvider p_161344_) {
         super(p_161343_, p_161344_);
@@ -27,19 +25,38 @@ public class PalmFoliagePlacer extends FoliagePlacer {
         return TuroFoliagePlacerType.PALM_FOLIAGE_PLACER.get();
     }
 
-    protected void createFoliage(LevelSimulatedReader WorldReader, BiConsumer<BlockPos, BlockState> AcceptedLocations, Random Rand, TreeConfiguration TreeConf, int p_161350_, FoliagePlacer.FoliageAttachment p_161351_, int p_161352_, int p_161353_, int p_161354_) {
-        boolean flag = p_161351_.doubleTrunk();
-        BlockPos blockpos = p_161351_.pos().above(p_161354_);
-        this.placeLeavesRow(WorldReader, AcceptedLocations, Rand, TreeConf, blockpos, p_161353_ + p_161351_.radiusOffset(), -1 - p_161352_, flag);
-        this.placeLeavesRow(WorldReader, AcceptedLocations, Rand, TreeConf, blockpos, p_161353_ - 1, -p_161352_, flag);
-        this.placeLeavesRow(WorldReader, AcceptedLocations, Rand, TreeConf, blockpos, p_161353_ + p_161351_.radiusOffset() - 1, 0, flag);
+    protected void createFoliage(@NotNull LevelSimulatedReader worldReader, @NotNull BiConsumer<BlockPos, BlockState> acceptedLocations, @NotNull Random random, @NotNull TreeConfiguration treeConfiguration, int p_161350_, FoliagePlacer.FoliageAttachment attachment, int height, int radius, int p_161354_) {
+        boolean flag = attachment.doubleTrunk();
+        // Layer 1
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_),radius - 2, height, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).north(1),radius - 2, height, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).south(1),radius - 2, height, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).west(1),radius - 2, height, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).east(1),radius - 2, height, flag);
+
+        //Layer 2
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_),radius - 1,height - 1, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).north(2),radius - 2, height - 1, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).south(2),radius - 2, height - 1, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).west(2),radius - 2, height - 1, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).east(2),radius - 2, height - 1, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).north(1).east(1),radius - 2, height - 1, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).north(1).west(1),radius - 2, height - 1, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).south(1).east(1),radius - 2, height - 1, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().above(p_161354_).south(1).west(1),radius - 2, height - 1, flag);
+
+        //Layer 3
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().north(3),radius - 2,   height - 2, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().south(3),radius - 2,   height - 2, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().west(3), radius - 2,   height - 2, flag);
+        this.placeLeavesRow(worldReader, acceptedLocations, random, treeConfiguration, attachment.pos().east(3), radius - 2,   height - 2, flag);
     }
 
-    public int foliageHeight(Random Rand, int p_68390_, TreeConfiguration TreeConf) {
+    public int foliageHeight(@NotNull Random random, int height, @NotNull TreeConfiguration treeConfiguration) {
         return 0;
     }
 
-    protected boolean shouldSkipLocation(Random p_68382_, int p_68383_, int p_68384_, int p_68385_, int p_68386_, boolean p_68387_) {
+    protected boolean shouldSkipLocation(@NotNull Random random, int p_68383_, int p_68384_, int p_68385_, int p_68386_, boolean p_68387_) {
         if (p_68384_ == 0) {
             return (p_68383_ > 1 || p_68385_ > 1) && p_68383_ != 0 && p_68385_ != 0;
         } else {

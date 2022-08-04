23f6fdd1ecbd80f8eecf47629077191e8b27faac
OleSTEEP <krinkinoleg@gmail.com>
2022-08-04 12:50:02 +0300

    Random palm type from sapling

--- src/main/java/com/olesteep/turorudi/world/feature/tree/PalmTreeGrower.java
+++ src/main/java/com/olesteep/turorudi/world/feature/tree/PalmTreeGrower.java
@@ -8,12 +8,17 @@ import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Random;
-
 public class PalmTreeGrower extends AbstractTreeGrower {
     @Nullable
     @Override
-    protected Holder<? extends ConfiguredFeature<?, ?>> getConfiguredFeature(@NotNull RandomSource p_204307_, boolean p_204308_) {
-        return TuroConfiguredFeatures.PALM_TREE;
+    protected Holder<? extends ConfiguredFeature<?, ?>> getConfiguredFeature(@NotNull RandomSource random, boolean p_204308_) {
+        float probability = 0.5F;
+
+        if (!(random.nextFloat() >= probability)) {
+            return TuroConfiguredFeatures.PALM_TREE_COCO;
+        }
+        else {
+            return TuroConfiguredFeatures.PALM_TREE_BANANA;
+        }
     }
 }

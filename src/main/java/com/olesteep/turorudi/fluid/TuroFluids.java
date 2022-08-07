56440dee28eea44aec192db400b3877e0d8013a4
OleSTEEP <krinkinoleg@gmail.com>
2022-08-07 19:19:02 +0300

    Fluid effects

--- src/main/java/com/olesteep/turorudi/fluid/TuroFluids.java
+++ src/main/java/com/olesteep/turorudi/fluid/TuroFluids.java
@@ -1,6 +1,7 @@
 package com.olesteep.turorudi.fluid;
 
 import com.olesteep.turorudi.TuroRudi;
+import com.olesteep.turorudi.fluid.block.CondensedMilkFluidBlock;
 import com.olesteep.turorudi.item.TuroItems;
 import com.olesteep.turorudi.block.TuroBlocks;
 import net.minecraft.resources.ResourceLocation;
@@ -28,8 +29,8 @@ public class TuroFluids {
 
     public static final RegistryObject<FlowingFluid> MILK_COND_FLUID = FLUIDS.register("milk_condensed_fluid", () -> new ForgeFlowingFluid.Source(TuroFluids.MILK_COND_PROPERTIES));
     public static final RegistryObject<FlowingFluid> MILK_COND_FLOWING = FLUIDS.register("milk_condensed_flowing", () -> new ForgeFlowingFluid.Flowing(TuroFluids.MILK_COND_PROPERTIES));
-    public static final ForgeFlowingFluid.Properties MILK_COND_PROPERTIES = new ForgeFlowingFluid.Properties(() -> MILK_COND_FLUID.get(), () -> MILK_COND_FLOWING.get(), FluidAttributes.builder(WATER_STILL_RL, WATER_FLOWING_RL).density(15).luminosity(2).viscosity(5).sound(SoundEvents.HONEY_DRINK).overlay(WATER_OVERLAY_RL).color(0xbf7a3816)).slopeFindDistance(2).levelDecreasePerBlock(2).block(() -> TuroFluids.MILK_COND_BLOCK.get()).bucket(() -> TuroItems.MILK_COND_BUCKET.get());
-    public static final RegistryObject<LiquidBlock> MILK_COND_BLOCK = registerBlockWithoutItem(TuroBlocks.BLOCKS,"milk_cond", () -> new LiquidBlock(() -> TuroFluids.MILK_COND_FLUID.get(), BlockBehaviour.Properties.of(Material.WATER).noCollission().strength(100f).noDrops()));
+    public static final ForgeFlowingFluid.Properties MILK_COND_PROPERTIES = new ForgeFlowingFluid.Properties(() -> MILK_COND_FLUID.get(), () -> MILK_COND_FLOWING.get(), FluidAttributes.builder(WATER_STILL_RL, WATER_FLOWING_RL).density(30).luminosity(2).viscosity(30).sound(SoundEvents.HONEY_DRINK).overlay(WATER_OVERLAY_RL).color(0xbf7a3816)).slopeFindDistance(2).levelDecreasePerBlock(2).block(() -> TuroFluids.MILK_COND_BLOCK.get()).bucket(() -> TuroItems.MILK_COND_BUCKET.get());
+    public static final RegistryObject<LiquidBlock> MILK_COND_BLOCK = registerBlockWithoutItem(TuroBlocks.BLOCKS,"milk_cond", () -> new CondensedMilkFluidBlock(() -> TuroFluids.MILK_COND_FLUID.get(), BlockBehaviour.Properties.of(Material.WATER).noCollission().strength(100f).noDrops()));
 
 public static void register(IEventBus eventBus) {
         FLUIDS.register(eventBus);

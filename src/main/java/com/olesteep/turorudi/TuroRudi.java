ddfba026c91bedeb96892d7374aba5ee1401e256
olesteep <krinkinoleg@gmail.com>
2022-10-23 21:23:22 +0300

    New curd snack's vendor

--- src/main/java/com/olesteep/turorudi/TuroRudi.java
+++ src/main/java/com/olesteep/turorudi/TuroRudi.java
@@ -49,6 +49,7 @@ public class TuroRudi {
         Corenovka.register(eventBus);
         Pottyos.register(eventBus);
         Preobrajenskiy.register(eventBus);
+        Rostagroexport.register(eventBus);
         Svitlogorie.register(eventBus);
         Vkusnoteevo.register(eventBus);
         Vologosha.register(eventBus);

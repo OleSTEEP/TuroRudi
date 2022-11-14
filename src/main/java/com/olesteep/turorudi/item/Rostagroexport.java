0073e035a18f7ead8432ae1b4cb657f31408bdeb
olesteep <krinkinoleg@gmail.com>
2022-11-14 09:40:14 +0300

    Rostagroexport Potato

--- src/main/java/com/olesteep/turorudi/item/Rostagroexport.java
+++ src/main/java/com/olesteep/turorudi/item/Rostagroexport.java
@@ -15,6 +15,8 @@ public class Rostagroexport {
     public static final RegistryObject<Item> ROSTAGRO_COCO = ITEMS.register("rostagroexport_coconut", () -> new Item(new Item.Properties().tab(TuroCreativeTabs.SNACKTAB).food(TuroFoods.TURO_COCO)));
     public static final RegistryObject<Item> ROSTAGRO_COND = ITEMS.register("rostagroexport_condensed", () -> new Item(new Item.Properties().tab(TuroCreativeTabs.SNACKTAB).food(TuroFoods.TURO_COND)));
 
+    public static final RegistryObject<Item> ROSTAGRO_POTATO = ITEMS.register("rostagroexport_potato", () -> new Item(new Item.Properties().tab(TuroCreativeTabs.SNACKTAB).food(TuroFoods.TURO_POTATO)));
+
     public static final RegistryObject<Item> PACKAGE_ROSTAGRO = ITEMS.register("package_rostagroexport", () -> new Item(new Item.Properties().tab(TuroCreativeTabs.ITEMTAB)));
 
     public static void register(IEventBus eventBus) {

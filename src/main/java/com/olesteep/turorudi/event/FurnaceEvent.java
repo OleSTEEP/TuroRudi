5f3b23a49f33a04c1a642c9797c38fcc77dd8a9b
OleSTEEP <krinkinoleg@gmail.com>
2023-01-26 20:44:42 +0300

    Initial fix for milk_fat craft

---
+++ src/main/java/com/olesteep/turorudi/event/FurnaceEvent.java
@@ -0,0 +1,22 @@
+package com.olesteep.turorudi.event;
+
+import com.olesteep.turorudi.TuroRudi;
+import com.olesteep.turorudi.item.TuroItems;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = TuroRudi.MOD_ID)
+public class FurnaceEvent {
+
+    @SubscribeEvent
+    public static void onSmelt(PlayerEvent.ItemSmeltedEvent event) {
+
+        if(event.getSmelting().getItem().equals(new ItemStack(TuroItems.MILK_FAT.get()).getItem())) {
+            event.getPlayer().addItem(new ItemStack(Items.BUCKET));
+
+        }
+    }
+}

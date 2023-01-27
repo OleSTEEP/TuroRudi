fa5e549808bf5b128cdf5208a0231d3da70ee9c5
OleSTEEP <krinkinoleg@gmail.com>
2023-01-27 17:48:25 +0300

    Register a HW block when the event has ended

--- src/main/java/com/olesteep/turorudi/registry/BlockRegisters.java
+++ src/main/java/com/olesteep/turorudi/registry/BlockRegisters.java
@@ -8,6 +8,8 @@ import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.function.Supplier;
 
 public class BlockRegisters {
@@ -20,6 +22,25 @@ public class BlockRegisters {
 
     public static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
         TuroItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(TuroCreativeTabs.ITEMTAB)));
+    }
+
+    public static <T extends Block> RegistryObject<T> registerEventBlock(DeferredRegister<Block> BLOCKS, String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerEventBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    public static <T extends Block> void registerEventBlockItem(String name, RegistryObject<T> block) {
+        DateTimeFormatter dtf_day = DateTimeFormatter.ofPattern("dd");
+        DateTimeFormatter dtf_month = DateTimeFormatter.ofPattern("MM");
+        LocalDateTime now = LocalDateTime.now();
+
+        if(Integer.parseInt(dtf_day.format(now)) >= 20 && Integer.parseInt(dtf_month.format(now)) == 10) {
+            TuroItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(TuroCreativeTabs.ITEMTAB)));
+        }
+        else {
+            TuroItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+        }
     }
 
     public static <T extends Block> RegistryObject<T> registerBlockWithoutItem(DeferredRegister<Block> BLOCKS, String name, Supplier<T> block) {

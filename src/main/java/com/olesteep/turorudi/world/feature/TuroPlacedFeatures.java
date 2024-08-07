15fac95dc46d036c5e00105ae25e0e06b83ff287
OleSTEEP <krinkinoleg@gmail.com>
2024-08-07 19:45:54 +0300

    Reduce features spawn rate

--- src/main/java/com/olesteep/turorudi/world/feature/TuroPlacedFeatures.java
+++ src/main/java/com/olesteep/turorudi/world/feature/TuroPlacedFeatures.java
@@ -12,7 +12,7 @@ public class TuroPlacedFeatures {
     public static final Holder<PlacedFeature> APRICOT_BUSH_PLACED = PlacementUtils.register(
             "apricot_bush_placed",
             TuroConfiguredFeatures.PATCH_APRICOT_BUSH,
-            RarityFilter.onAverageOnceEvery(16),
+            RarityFilter.onAverageOnceEvery(64),
             InSquarePlacement.spread(),
             PlacementUtils.HEIGHTMAP,
             BiomeFilter.biome()
@@ -20,7 +20,7 @@ public class TuroPlacedFeatures {
     public static final Holder<PlacedFeature> BLBERRY_BUSH_PLACED = PlacementUtils.register(
             "blueberry_bush_placed",
             TuroConfiguredFeatures.PATCH_BLBERRY_BUSH,
-            RarityFilter.onAverageOnceEvery(32),
+            RarityFilter.onAverageOnceEvery(64),
             InSquarePlacement.spread(),
             PlacementUtils.HEIGHTMAP,
             BiomeFilter.biome()
@@ -28,12 +28,12 @@ public class TuroPlacedFeatures {
     public static final Holder<PlacedFeature> CHERRY_PLACED = PlacementUtils.register(
             "cherry_placed",
             TuroConfiguredFeatures.CHERRY_SPAWN,
-            VegetationPlacements.treePlacement(PlacementUtils.countExtra(0, 0.0125f, 1))
+            VegetationPlacements.treePlacement(PlacementUtils.countExtra(0, 0.01f, 1))
     );
     public static final Holder<PlacedFeature> LEMON_BUSH_PLACED = PlacementUtils.register(
             "lemon_bush_placed",
             TuroConfiguredFeatures.PATCH_LEMON_BUSH,
-            RarityFilter.onAverageOnceEvery(16),
+            RarityFilter.onAverageOnceEvery(64),
             InSquarePlacement.spread(),
             PlacementUtils.HEIGHTMAP,
             BiomeFilter.biome()
@@ -41,7 +41,7 @@ public class TuroPlacedFeatures {
     public static final Holder<PlacedFeature> LIME_BUSH_PLACED = PlacementUtils.register(
             "lime_bush_placed",
             TuroConfiguredFeatures.PATCH_LIME_BUSH,
-            RarityFilter.onAverageOnceEvery(16),
+            RarityFilter.onAverageOnceEvery(64),
             InSquarePlacement.spread(),
             PlacementUtils.HEIGHTMAP,
             BiomeFilter.biome()
@@ -49,7 +49,7 @@ public class TuroPlacedFeatures {
     public static final Holder<PlacedFeature> ORANGE_PLACED = PlacementUtils.register(
             "orange_placed",
             TuroConfiguredFeatures.ORANGE_SPAWN,
-            VegetationPlacements.treePlacement(PlacementUtils.countExtra(0, 0.25f, 1))
+            VegetationPlacements.treePlacement(PlacementUtils.countExtra(0, 0.01f, 1))
     );
     public static final Holder<PlacedFeature> PALM_PLACED = PlacementUtils.register(
             "palm_placed",
